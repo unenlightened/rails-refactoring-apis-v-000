58e30a780fef319aa45fba4ed683dd1a71a71efc
Chely Ho <adept_inept@yahoo.com>
2018-07-17 04:33:52 +0000

    Done.

--- app/services/github_service.rb
+++ app/services/github_service.rb
@@ -1,33 +1,41 @@
 class GithubService
+  attr_reader :access_token, :repos
 
-  attr_reader :access_token
-
-  def initialize(access_hash = nil)
-    @access_token = access_hash["access_token"] if access_hash
+  def initialize(access_hash = {})
+    @access_token = access_hash["access_token"]
+    @repos = []
   end
 
   def authenticate!(client_id, client_secret, code)
     response = Faraday.post "https://github.com/login/oauth/access_token",
-        {client_id: client_id, client_secret: client_secret, code: code},
-        {'Accept' => 'application/json'}
-    access_hash = JSON.parse(response.body)
-    @access_token = access_hash["access_token"]
+      {client_id: client_id, client_secret: client_secret, code: code},
+      {'Accept' => 'application/json'}
+    json = JSON.parse(response.body)
+    @access_token = json["access_token"]
   end
 
   def get_username
-    user_response = Faraday.get "https://api.github.com/user", {}, {'Authorization' => "token #{self.access_token}", 'Accept' => 'application/json'}
-    user_json = JSON.parse(user_response.body)
-    user_json["login"]
+    response = Faraday.get "https://api.github.com/user", {},
+      {'Authorization' => "token #{access_token}", 'Accept' => 'application/json'}
+    json = JSON.parse(response.body)
+    json["login"]
   end
 
   def get_repos
-    response = Faraday.get "https://api.github.com/user/repos", {}, {'Authorization' => "token #{self.access_token}", 'Accept' => 'application/json'}
-    repos_array = JSON.parse(response.body)
-    repos_array.map{|repo| GithubRepo.new(repo) }
+    response = Faraday.get "https://api.github.com/user/repos", {},
+      {'Authorization' => "token #{access_token}", 'Accept' => 'application/json'}
+    json = JSON.parse(response.body)
+    build_repos(json)
   end
 
-  def create_repo(name)
-    Faraday.post "https://api.github.com/user/repos", {name: name}.to_json, {'Authorization' => "token #{self.access_token}", 'Accept' => 'application/json'}
+  def build_repos(json)
+    json.each do |repo_hash|
+      @repos << GithubRepo.new(repo_hash)
+    end
+    @repos
   end
 
+  def create_repo
+
+  end
 end
